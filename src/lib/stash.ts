12775bc9760ab97eb4143361212840666ac5ca79
Rob Sioss <39536918+rsioss@users.noreply.github.com>
2022-11-02 13:55:42 -0400

    feat: ensure trading partner keyspace is empty when re-running `configure-partners`

--- src/lib/stash.ts
+++ src/lib/stash.ts
@@ -1,8 +1,12 @@
 import pRetry from "p-retry";
 
 import {
-  CreateKeyspaceCommand, GetKeyspaceCommand,
+  CreateKeyspaceCommand,
+  DeleteKeyspaceCommand,
+  GetKeyspaceCommand,
+  Keyspace,
   KeyspaceAlreadyExistsError,
+  KeyspaceNotFoundError,
   StashClient,
   StashClientConfig
 } from "@stedi/sdk-client-stash";
@@ -23,36 +27,79 @@ export const stashClient = (): StashClient => {
   return _stashClient;
 };
 
-export const ensureKeyspaceExists = async (keyspaceName: string): Promise<void> => {
-  try {
-    const createKeyspaceResult = await stashClient().send(
-      new CreateKeyspaceCommand({ keyspaceName })
+export const createKeyspace = async (keyspaceName: string): Promise<Keyspace> => {
+  let keyspace: Keyspace = await stashClient().send(
+    new CreateKeyspaceCommand({ keyspaceName })
+  )
+
+  console.log(`${keyspaceName} keyspace status: ${keyspace.status}`);
+  if (keyspace.status !== "ACTIVE") {
+    console.log(`waiting for ${keyspaceName} keyspace to become ACTIVE`);
+    const getKeyspaceCommand = new GetKeyspaceCommand({ keyspaceName });
+
+    await pRetry(
+      async () => {
+        keyspace = await stashClient().send(getKeyspaceCommand);
+        if (keyspace.status !== "ACTIVE") {
+          throw new Error(`${keyspaceName} keyspace not active`);
+        }
+      },
+      {
+        retries: 4,
+        minTimeout: 1000,
+        maxTimeout: 5000,
+      }
     );
+  }
 
-    console.log(`${keyspaceName} keyspace status: ${createKeyspaceResult.status}`);
-    if (createKeyspaceResult.status !== "ACTIVE") {
-      console.log(`waiting for ${keyspaceName} keyspace to become ACTIVE`);
-      const getKeyspaceCommand = new GetKeyspaceCommand({ keyspaceName });
+  console.log(`${keyspaceName} keyspace created successfully`);
+  return keyspace;
+};
 
-      await pRetry(
-        async () => {
-          const getKeyspaceResponse = await stashClient().send(getKeyspaceCommand);
-          if (getKeyspaceResponse.status !== "ACTIVE") {
-            throw new Error(`keyspace not active: ${keyspaceName}`);
-          }
-        },
-        {
-          retries: 4,
-          minTimeout: 1000,
-          maxTimeout: 5000,
-        });
-    }
+export const deleteKeyspace = async (keyspaceName: string): Promise<void> => {
+  let keyspace: Keyspace = await stashClient().send(
+    new DeleteKeyspaceCommand({ keyspaceName })
+  )
+
+  console.log(`waiting for ${keyspaceName} keyspace deletion to finish`);
+  const getKeyspaceCommand = new GetKeyspaceCommand({ keyspaceName });
+
+  try {
+    await pRetry(
+      async () => {
+        keyspace = await stashClient().send(getKeyspaceCommand);
+        // when the deletion has been successful, the get command will throw KeyspaceNotFoundError
+        throw new Error(`${keyspaceName} keyspace not finished deleting`);
+      },
+      {
+        retries: 4,
+        minTimeout: 1000,
+        maxTimeout: 5000,
+      }
+    );
   } catch (e) {
-    // if keyspace already exists, just return
-    if (e instanceof KeyspaceAlreadyExistsError) {
+    if (e instanceof KeyspaceNotFoundError) {
+      console.log(`${keyspaceName} keyspace deleted successfully`);
       return;
     }
 
     throw e;
+  }
+};
+
+// Create new keyspace if it doesn't already exist, otherwise delete existing
+// keyspace and create a new one to ensure that it is in an empty state
+export const ensureEmptyKeyspaceExists = async (keyspaceName: string): Promise<void> => {
+  try {
+    await createKeyspace(keyspaceName);
+  } catch (e) {
+    // re-throw anything other than KeyspaceAlreadyExistsError
+    if (!(e instanceof KeyspaceAlreadyExistsError)) {
+      throw e;
+    }
+
+    console.log(`${keyspaceName} keyspace already exists; deleting and re-creating to ensure empty state`);
+    await deleteKeyspace(keyspaceName);
+    await createKeyspace(keyspaceName);
   }
 };
--- src/setup/configureTradingPartners.ts
+++ src/setup/configureTradingPartners.ts
@@ -3,7 +3,7 @@ import * as fs from "fs";
 import { SetValueCommand } from "@stedi/sdk-client-stash";
 
 import { Convert } from "../lib/types/TradingPartnerList.js";
-import { ensureKeyspaceExists, stashClient } from "../lib/stash.js";
+import { ensureEmptyKeyspaceExists, stashClient } from "../lib/stash.js";
 import { TRADING_PARTNERS_KEYSPACE_NAME } from "../lib/constants.js";
 
 const TRADING_PARTNER_LIST_INPUT_FILE = "./src/resources/tradingPartners/tradingPartnerList.json";
@@ -12,7 +12,10 @@ const TRADING_PARTNER_LIST_INPUT_FILE = "./src/resources/tradingPartners/trading
   const configJson = fs.readFileSync(TRADING_PARTNER_LIST_INPUT_FILE, "utf8");
   const tradingPartnerList = Convert.toTradingPartnerList(configJson);
 
-  await ensureKeyspaceExists(TRADING_PARTNERS_KEYSPACE_NAME);
+  // ensure that initial state is an empty keyspace so that the contents of trading partner
+  // input file represents the state of all trading partners (don't leave outdated entries
+  // behind when script is re-run)
+  await ensureEmptyKeyspaceExists(TRADING_PARTNERS_KEYSPACE_NAME);
 
   const promises = tradingPartnerList.items.map(async (tradingPartner) => {
     console.log(`processing trading partner: ${tradingPartner.key} (${tradingPartner.value.name})`);

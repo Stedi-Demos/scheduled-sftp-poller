a4c590472c947051909b071a556c39aa6f7d5b8d
Olaf Conijn <olaf_conijn@hotmail.com>
2023-01-30 12:56:49 +0100

    chore: update fromCredentials with fromAwsCredentialIdentity

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,11 +1,11 @@
 import dotenv from "dotenv";
-import { fromCredentials } from "@stedi/sdk-token-provider";
+import { fromAwsCredentialIdentity } from "@stedi/sdk-token-provider-aws-identity";
 
 dotenv.config({ override: true });
 
 const credentials = process.env.STEDI_API_KEY
   ? { apiKey: process.env.STEDI_API_KEY }
-  : { token: fromCredentials() };
+  : { token: fromAwsCredentialIdentity() };
 
 export const DEFAULT_SDK_CLIENT_PROPS = {
   ...credentials,
